3c2bd8985487d68ed8ce369501d52aab81eb9539
Stewie1 <xionone@web.de>
2023-07-19 09:26:43 +0200

    rename button regarding there usage, implement display of pressed keys in screen, implement basic logic of different eventhandling regarding keys

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
 const touchpad = document.getElementById('touchpad');
+const screen = document.querySelector('#screen p');
+let current = "";
 
 function handleButtonClick(event){
     const button = event.target;
-    const value = button.textContent
-    console.log(`Button Value: ${value}`);
+    current += button.textContent;
 }
 
 touchpad.addEventListener('click', function(event){
-    if (event.target.classList.contains('solo-key')){
+    if (event.target.classList[0].endsWith('-key')){
+        const pressedKey = event.target
+        console.log(pressedKey);
+        switch (pressedKey){
+            case 'num-key':
+                handleButtonClick();
+                break;
+            case 'math-key':
+                //handleMathOperation();
+                break;
+            case 'result-key':
+                //handleResult;
+            case 'delete-key':
+                handle_delete();
+            case 'delete-all-key':
+                handle_delete_all();
+        };
+        
         handleButtonClick(event);
-    }
+    };
+    update_screen(current);
 })
+
+function update_screen(current) {
+    screen.innerText = current;
+}
+
+function handleMathOperation(){
+    return;
+}
+
+function handle_delete_all(){
+    return current = "";
+}
+
+function handle_delete(){
+    current = current.slice(0, -1);
+}
