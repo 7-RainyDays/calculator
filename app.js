0bbb918ecd5a76d309e720988d3375b62968ac0d
Stewie1 <xionone@web.de>
2023-07-19 13:35:34 +0200

    remove square and **, change to ( and ), implement api call for result

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
 const touchpad = document.getElementById('touchpad');
 const screen = document.querySelector('#screen p');
+let baseURL = "http://api.mathjs.org/v4/?expr=";
 let current = "";
 
 function handleButtonClick(event){
     const button = event.target;
     current += button.textContent;
+    return current;
 }
 
 touchpad.addEventListener('click', function(event){
     if (event.target.classList[0].endsWith('-key')){
-        const pressedKey = event.target
-        console.log(pressedKey);
-        switch (pressedKey){
+        const pressedKeyGroup = event.target.classList[0];
+        const pressedKey = event.target.textContent;
+        switch (pressedKeyGroup){
             case 'num-key':
-                handleButtonClick();
+                handleButtonClick(event);
                 break;
             case 'math-key':
-                //handleMathOperation();
+                handleMathOperation(pressedKey);
                 break;
             case 'result-key':
-                //handleResult;
+                fetch_result();
+                break;
             case 'delete-key':
                 handle_delete();
+                break
             case 'delete-all-key':
                 handle_delete_all();
+                break;
         };
-        
-        handleButtonClick(event);
     };
-    update_screen(current);
+    update_screen();
 })
 
-function update_screen(current) {
+function update_screen() {
     screen.innerText = current;
 }
 
-function handleMathOperation(){
-    return;
+function handleMathOperation(pressedKey){
+    current += pressedKey;
 }
 
 function handle_delete_all(){
@@ -44,5 +47,17 @@ function handle_delete_all(){
 }
 
 function handle_delete(){
-    current = current.slice(0, -1);
+    if (current !== ""){
+        current = current.slice(0, -1);
+    };
+}
+
+function fetch_result() {
+    const encoded = encodeURIComponent(current);
+    fetch('http://api.mathjs.org/v4/?expr='+ encoded)
+        .then(response => response.json())
+        .then(data => {
+            current = data;
+            update_screen();
+    })
 }
